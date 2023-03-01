3c1a004559fae17183b5e647b40d9c3a989961d7
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-03-01 17:26:51 +0900

    ;

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from timer import timer
-from pino import search, login
+from pino import search, login, github_login
 import os
 
 
@@ -18,6 +18,7 @@ def search_func():
     url = url_entry.get()
     print("2")
     # http://gw.meritium.co.kr
+    # https://github.com/sinde530/leave_data
     search(url)
     print("3")
 
@@ -26,6 +27,11 @@ def login_information():
     username = login_id_input.get()
     userpassword = login_pw_input.get()
     login(username, userpassword)
+
+    github_url = github_url_entry.get()
+    github_id = github_id_input.get()
+    github_pw = github_pw_input.get()
+    github_login(github_url, github_id, github_pw)
 
 
 def create_window():
@@ -36,6 +42,10 @@ def create_window():
     global second
     global login_id_input
     global login_pw_input
+    global github_url_entry
+    global github_id_input
+    global github_pw_input
+    global url_value_save
 
     root = Tk()
     root.geometry("800x600")
@@ -45,6 +55,7 @@ def create_window():
     hour = StringVar()
     minute = StringVar()
     second = StringVar()
+    url_value_save = StringVar()
 
     hour.set("00")
     minute.set("00")
@@ -68,28 +79,46 @@ def create_window():
     )
     timer_button.place(x=70, y=120)
 
-    url_frame = Frame(root, pady=24)
-    url_frame.pack()
-    url_label = Label(url_frame, text="Enter URL:")
-    url_label.grid(row=0, column=0)
-    url_entry = Entry(url_frame)
-    url_entry.grid(row=0, column=1)
-
     reboot_frame = Frame(root)
     reboot_frame.pack()
     restart_button = Button(reboot_frame, text="ReBoot", width=15, command=restart)
-    restart_button.grid(row=1, column=1)
+    restart_button.grid(row=0, column=0, pady=16)
 
-    login_frame = Frame(root, pady=16)
+    url_frame = Frame(root)
+    url_frame.pack()
+    url_label = Label(url_frame, text="Enter URL:")
+    url_label.grid(row=1, column=0)
+    url_entry = Entry(url_frame, textvariable=url_value_save)
+    url_entry.grid(row=1, column=1)
+
+    login_frame = Frame(root)
     login_frame.pack()
     login_id_label = Label(login_frame, text="Email or ID")
-    login_id_label.grid(row=1, column=0)
+    login_id_label.grid(row=2, column=0)
     login_id_input = Entry(login_frame)
-    login_id_input.grid(row=1, column=1)
+    login_id_input.grid(row=2, column=1)
     login_pw_labe = Label(login_frame, text="Password")
-    login_pw_labe.grid(row=2, column=0)
+    login_pw_labe.grid(row=3, column=0)
     login_pw_input = Entry(login_frame)
-    login_pw_input.grid(row=2, column=1)
+    login_pw_input.grid(row=3, column=1)
+
+    github_frame = Frame(root, pady=30)
+    github_frame.pack()
+
+    github_url = Label(github_frame, text="github URL:")
+    github_url.grid(row=5, column=0)
+    github_url_entry = Entry(github_frame)
+    github_url_entry.grid(row=5, column=1)
+
+    github_id_label = Label(github_frame, text="github ID")
+    github_id_label.grid(row=6, column=0)
+    github_id_input = Entry(github_frame)
+    github_id_input.grid(row=6, column=1)
+
+    github_pw_label = Label(github_frame, text="github PW")
+    github_pw_label.grid(row=7, column=0)
+    github_pw_input = Entry(github_frame)
+    github_pw_input.grid(row=7, column=1)
 
 
 create_window()
--- tkinter/pino.py
+++ tkinter/pino.py
@@ -1,4 +1,5 @@
 import time
+import os
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
@@ -11,6 +12,7 @@ from selenium import webdriver
 
 browser = webdriver.Chrome()
 modal_close = Alert(browser)
+now = datetime.now()
 
 
 def search(url):
@@ -33,6 +35,7 @@ def login(username, password):
 
         login_btn = browser.find_element(By.ID, "loginBtn")
         login_btn.click()
+        browser.implicitly_wait(60)
         time.sleep(2)
 
         # Start
@@ -47,11 +50,12 @@ def login(username, password):
         # go_to_leave_btn.click()
 
         # time.sleep(1)
-        modal_close.accept()
+        # modal_close.accept()
         # time.sleep(2)
 
         my_desk_btn = browser.find_element(By.XPATH, "//*[@id='MainNav']/li[6]/a")
         my_desk_btn.click()
+        browser.implicitly_wait(60)
 
         # ctrl a + c
         ActionChains(browser).key_down(Keys.CONTROL).send_keys("a").send_keys(
@@ -65,3 +69,67 @@ def login(username, password):
     finally:
         time.sleep(2)
         print("successfully Login")
+
+
+def github_login(github_url, github_name, github_password):
+    now_date = datetime.fromordinal(datetime.today().toordinal()).strftime(
+        "day: %Y 년/%m 월/%d 일"
+    )
+    try:
+        print("social login start")
+        browser.get(github_url)
+        browser.maximize_window()
+        browser.implicitly_wait(60)
+
+        user_id = browser.find_element(By.ID, "login_field")
+        user_id.send_keys(github_name)
+        time.sleep(1)
+
+        user_pw = browser.find_element(By.ID, "password")
+        user_pw.send_keys(github_password)
+        time.sleep(1)
+
+        login_btn = browser.find_element(
+            By.XPATH,
+            "/html/body/div[1]/div[1]/header/div/div[2]/div/div/div[2]/div/div/form/div/input[11]",
+        )
+        login_btn.click()
+        browser.implicitly_wait(60)
+        time.sleep(2)
+
+        add_file_button = browser.find_element(
+            By.XPATH,
+            "//*[@id='repo-content-pjax-container']/div/div/div[3]/div[1]/div[2]/details",
+        )
+        add_file_button.click()
+        time.sleep(1)
+
+        create_new_file_button = browser.find_element(
+            By.XPATH,
+            "//*[@id='repo-content-pjax-container']/div/div/div[3]/div[1]/div[2]/details/div/ul/li[3]/form/button",
+        )
+        create_new_file_button.click()
+        browser.implicitly_wait(60)
+        time.sleep(1)
+
+        title_input = browser.find_element(
+            By.XPATH,
+            "//*[@id='repo-content-pjax-container']/div/div/form[2]/div/div[1]/span/input",
+        )
+        title_input.send_keys(now_date)
+        time.sleep(1)
+
+        description = browser.find_element(By.XPATH, "//*[@id='code-editor']/div/pre")
+        description.send_keys(Keys.CONTROL, "v")
+        time.sleep(1)
+
+        commit_new_file_button = browser.find_element(By.ID, "submit-file")
+        commit_new_file_button.click()
+
+        time.sleep(5)
+        browser.quit()
+
+        os.system("shutdown /h")
+
+    except NoSuchElementException as error:
+        print(error)
