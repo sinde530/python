f2a752bc9b3529508e07487a6acda7ac44b2ea0f
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-03-26 16:18:08 +0900

    flask test

--- pino-chatbot/flask_test.py
+++ pino-chatbot/flask_test.py
@@ -1,5 +1,6 @@
-import json
 import random
+import json
+import datetime
 from flask import Flask, request, render_template
 from flask_cors import CORS, cross_origin
 from nltk.chat.util import Chat, reflections
@@ -8,20 +9,23 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
-# load the chatbot data
+current_date = datetime.datetime.now().strftime("%A, %B %d, %Y")
+current_time = datetime.datetime.now().strftime("%H:%M:%S")
+
 pairs = [
     ["hi", ["Hello!", "Hi there!"]],
     ["what is your name?", ["My name is Chatbot."]],
     ["bye", ["Goodbye!", "Bye!"]],
+    ["what is the current date?", [f"The current date is {current_date}."]],
+    ["what is the current time?", [f"The current time is {current_time}."]],
 ]
 with open("data.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
-# define chatbot functions
 user_inputs = []
 
 
-def chatbot_response(user_input):
+def chatbot_response(user_input, confirm_message, new_data):
     bot_response = ""
     if user_input:
         chatbot = Chat(pairs, reflections)
@@ -36,27 +40,23 @@ def chatbot_response(user_input):
         else:
             bot_response = "I'm sorry, I'm not sure. Please try asking a different question or providing more information."
 
-        if (
-            "Please try asking a different question or providing more information."
-            in bot_response
-        ):
-            confirm_message = "I don't have an answer for that. Can you tell me?"
-            user_confirm = input(confirm_message)
-
-            if user_confirm.lower() == "yes":
-                try:
-                    new_data = input("What's the answer to your question?")
-                    data[user_input] = new_data
-                    with open("data.json", "w", encoding="utf-8") as f:
-                        json.dump(data, f, ensure_ascii=False)
-                    bot_response = "Thank you! I've added that to my knowledge base."
-                except Exception as e:
-                    bot_response = "I'm sorry, there was an error processing your request: {}".format(
-                        e
-                    )
+            if confirm_message:
+                if confirm_message.lower() == "yes":
+                    if new_data:
+                        data[user_input] = new_data
+                        with open("data.json", "w", encoding="utf-8") as f:
+                            json.dump(data, f, ensure_ascii=False)
+                        bot_response = (
+                            "Thank you! I've added that to my knowledge base."
+                        )
+                    else:
+                        bot_response = "I'm sorry, I didn't receive any new data. Please try again."
+                else:
+                    bot_response = "I'm sorry, I can't help with that."
             else:
-                bot_response = "I'm sorry, I can't help with that."
+                bot_response = "I'm not sure what you mean. Do you want to add this to my knowledge base?"
 
+    else:
         save_user_input(user_input, bot_response)
 
     return bot_response
@@ -78,18 +78,16 @@ def index():
 @cross_origin()
 def chat():
     user_input = request.form.get("user_input")
+    confirm_message = request.form.get("confirm_message")
+    new_data = request.form.get("new_data")
     bot_response = ""
     if user_input:
-        bot_response = chatbot_response(user_input)
+        bot_response = chatbot_response(user_input, confirm_message, new_data)
         response = {"bot_response": bot_response}
-        print("else user_input", user_input)
-        print("else bot_response", response)
     else:
         response = {
             "bot_response": "I'm sorry, I did not receive any input. Please try again."
         }
-        print("else user_input", user_input)
-        print("else bot_response", bot_response)
     return response
 
 
