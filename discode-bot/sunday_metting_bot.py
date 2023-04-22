7c7566ec6cc129adaf7b856ede292a3698cfe230
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-04-22 17:25:28 +0900

    Add scheduled_message wait function

--- discode-bot/sunday_metting_bot.py
+++ discode-bot/sunday_metting_bot.py
@@ -10,24 +10,33 @@ load_dotenv()
 sunday_api = os.environ.get("SUNDAY_METTING_API")
 sunday_channel_id = os.environ.get("SUNDAY_CHANNEL_ID")
 
+print("sunday_api: ", sunday_api)
+print("sunday_channel_id: ", sunday_channel_id)
+
 TOKEN = sunday_api
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
+print("client: ", client)
 
 scheduler = AsyncIOScheduler()  # APScheduler 스케줄러 생성
 
 
 # 일요일 9시 30분 (KST)로 스케줄링
-@scheduler.scheduled_job('cron', day_of_week='sun', hour=9, minute=30, second=0, timezone='Asia/Seoul')
+@scheduler.scheduled_job(
+    "cron", day_of_week="sun", hour=9, minute=30, second=0, timezone="Asia/Seoul"
+)
 async def scheduled_message():
-    channel = client.get_channel(sunday_channel_id)  # 알림을 보낼 디스코드 채널 ID 입력
-    await channel.send("매주 일요일 10시에 회의가 있습니다. 참석해주세요!")  # 알림 메시지 내용 입력
+    await client.wait_until_ready()
+    channel = client.get_channel(int(sunday_channel_id))  # 알림을 보낼 디스코드 채널 ID 입력
+    await channel.send("@everyone 매주 일요일 10시에 회의가 있습니다. 참석해주세요!")  # 알림 메시지 내용 입력
 
 
 @client.event
 async def on_ready():
-    print(f'{client.user}로 로그인하였습니다.')
+    print(f"{client.user}로 로그인하였습니다.")
     print(client.user.id)
+
     scheduler.start()
+
 
 client.run(TOKEN)
